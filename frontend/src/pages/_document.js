8772192c002a0c98f80d8a6a07d6637d917fa410
clinicaespacodorenascer-tech <clinicaespacodorenascer@gmail.com>
2026-09-02 12:44:03 -0300

    Create custom Document component for Next.js

---
+++ frontend/src/pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+        <link rel="icon" href="/logo.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/logo.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
