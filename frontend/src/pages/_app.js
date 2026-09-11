ac251e446fe9a804d2b96b8fcc502ea89f2a1421
clinicaespacodorenascer-tech <clinicaespacodorenascer@gmail.com>
2026-09-11 00:10:45 -0300

    Update _app.js

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,5 +1,14 @@
+import { useEffect } from "react";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+  // Registra o service worker (necessário pro navegador oferecer "Instalar app"/"Adicionar à
+  // tela de início"). Só roda no navegador, nunca no build do servidor.
+  useEffect(() => {
+    if (typeof window !== "undefined" && "serviceWorker" in navigator) {
+      navigator.serviceWorker.register("/sw.js").catch(() => {});
+    }
+  }, []);
+
   return <Component {...pageProps} />;
 }
